06a41bb61de39fca9ea0c1239f15f5c4c86c5db0
Emilio Ziniades <emilioziniades@protonmail.com>
2023-02-26 21:49:54 +0200

    listen to clippy

--- src/build.rs
+++ src/build.rs
@@ -17,30 +17,28 @@ pub fn build(cwd: PathBuf) -> Result<(), Error> {
     for entry in WalkDir::new(&dirs.content)
         .into_iter()
         .filter_map(|entry| entry.ok())
-        .filter(|entry| !entry.file_type().is_dir())
+        .filter(|entry| entry.file_type().is_file())
     {
         let content_path = entry.path();
         debug!("content file: {content_path:#?}");
 
         let template_path = dirs.find_template_path(content_path)?;
 
-        let build_path = dirs.make_build_path(&template_path, &content_path);
+        let build_path = dirs.make_build_path(&template_path, content_path);
         debug!("build path: {build_path:?}");
 
         let tera_template_name = dirs.tera_template_name(&template_path);
         let tera_template_name = tera
             .get_template_names()
             .find(|name| name == &tera_template_name)
-            .ok_or(io::Error::new(
-                io::ErrorKind::NotFound,
-                format!("{tera_template_name:?}"),
-            ))?;
+            .ok_or_else(|| {
+                io::Error::new(io::ErrorKind::NotFound, format!("{tera_template_name:?}"))
+            })?;
 
         debug!("tera template found {tera_template_name}");
 
         let page_data: PageData = fs::read_to_string(content_path)?.try_into()?;
-
-        trace!("parsed markdown: {page_data:#?}");
+        trace!("parsed page data: {page_data:#?}");
         let context = tera::Context::from(page_data);
         trace!("tera context: {context:#?}");
 
